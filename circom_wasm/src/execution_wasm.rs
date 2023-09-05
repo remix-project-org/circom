52e8e3fbd9590920cb90222b7315899800cddb09
ioedeveloper <daviddisu8@gmail.com>
2023-09-05 11:36:45 +0000

    Separate generate r1cs

--- circom_wasm/src/execution_wasm.rs
+++ circom_wasm/src/execution_wasm.rs
@@ -1,16 +1,14 @@
 use std::rc::Rc;
-
 use compiler::hir::very_concrete_program::VCP;
 use constraint_generation::{BuildConfig, ConstraintWriter, FlagsExecution, instantiation, export, sync_dag_and_vcp};
 use constraint_list::ConstraintList;
-use constraint_writers::debug_writer::DebugWriter;
 use dag::DAG;
 use program_structure::program_archive::ProgramArchive;
+use constraint_writers::ConstraintExporter;
 
 use crate::error_reporting_wasm::print_reports;
 
 pub struct ExecutionConfig {
-    pub r1cs: String,
     pub sym: String,
     pub json_constraints: String,
     pub no_rounds: usize,
@@ -21,7 +19,6 @@ pub struct ExecutionConfig {
     pub flag_verbose: bool,
     pub inspect_constraints_flag: bool,
     pub sym_flag: bool,
-    pub r1cs_flag: bool,
     pub json_substitution_flag: bool,
     pub json_constraint_flag: bool,
     pub prime: String,
@@ -30,8 +27,7 @@ pub struct ExecutionConfig {
 pub fn execute_project(
     program_archive: ProgramArchive,
     config: ExecutionConfig,
-) -> Result<VCP, Vec<String>> {
-    let debug = DebugWriter::new(config.json_constraints).unwrap();
+) -> Result<(ConstraintWriter, VCP), Vec<String>> {
     let build_config = BuildConfig {
         no_rounds: config.no_rounds,
         flag_json_sub: config.json_substitution_flag,
@@ -45,16 +41,13 @@ pub fn execute_project(
     };
     let custom_gates = program_archive.custom_gates;
     let (exporter, vcp) = build_circuit_wasm(program_archive, build_config)?;
-    // if config.r1cs_flag {
-    //     generate_output_r1cs(&config.r1cs, exporter.as_ref(), custom_gates)?;
-    // }
     // if config.sym_flag {
     //     generate_output_sym(&config.sym, exporter.as_ref())?;
     // }
     // if config.json_constraint_flag {
     //     generate_json_constraints(&debug, exporter.as_ref())?;
     // }
-    Result::Ok(vcp)
+    Result::Ok((exporter, vcp))
 }
 
 fn build_circuit_wasm(program: ProgramArchive, config: BuildConfig) -> Result<(ConstraintWriter, VCP), Vec<String>> {
@@ -113,15 +106,15 @@ fn simplification_process_wasm(vcp: &mut VCP, dag: DAG, config: &BuildConfig) ->
     list
 }
 
-// fn generate_output_r1cs(file: &str, exporter: &dyn ConstraintExporter, custom_gates: bool) -> Result<(), ()> {
-//     if let Result::Ok(()) = exporter.r1cs(file, custom_gates) {
-//         println!("{} {}", Colour::Green.paint("Written successfully:"), file);
-//         Result::Ok(())
-//     } else {
-//         eprintln!("{}", Colour::Red.paint("Could not write the output in the given path"));
-//         Result::Err(())
-//     }
-// }
+pub fn generate_output_r1cs(exporter: &dyn ConstraintExporter, custom_gates: bool) -> Result<Vec<u8>, Vec<String>> {
+    if let Result::Ok(()) = exporter.r1cs(file, custom_gates) {
+        // println!("{} {}", Colour::Green.paint("Written successfully:"), file);
+        Result::Ok(())
+    } else {
+        // eprintln!("{}", Colour::Red.paint("Could not write the output in the given path"));
+        Result::Err(vec!["Could not write the output in the given path".to_string()])
+    }
+}
 
 // fn generate_output_sym(file: &str, exporter: &dyn ConstraintExporter) -> Result<(), ()> {
 //     if let Result::Ok(()) = exporter.sym(file) {
--- circom_wasm/src/lib.rs
+++ circom_wasm/src/lib.rs
@@ -12,6 +12,7 @@ use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsValue;
 
 use crate::compilation_wasm::CompilerConfig;
+use crate::execution_wasm::generate_output_r1cs;
 
 struct CircuitConfig {
     prime: String
@@ -84,11 +85,48 @@ pub fn parse(file_name: String, sources: JsValue) -> String {
     }
 }
 
+#[wasm_bindgen]
+pub fn generate_r1cs(file_name: String, sources: JsValue) -> Vec<u8> {
+    if let Some(config) = config.dyn_into::<js_sys::Object>().ok() {
+        let prime: JsValue = js_sys::Reflect::get(&config, &"prime".into()).unwrap();
+        let prime = prime.as_string().unwrap();
+        let circuit_config = CircuitConfig {
+            prime
+        };
+        let mut link_libraries = Vec::new();
+        let mut link_libraries_sources = Vec::new();
+
+        if let Ok(file_sources) = sources.dyn_into::<js_sys::Object>() {
+            let source_keys = js_sys::Object::keys(&(file_sources));
+            let source_keys = source_keys.iter().map(|key| key.as_string().unwrap());
+            let source_values = js_sys::Object::values(&file_sources);
+            let source_values = source_values.iter().map(|value| value.as_string().unwrap_or_else(|| String::from("")));
+
+            link_libraries.extend(source_keys);
+            link_libraries_sources.extend(source_values);
+        }
+        let result = start_compiler(file_name, link_libraries, link_libraries_sources, circuit_config);
+
+        match result {
+            Result::Err(report) => {
+                // println!("{}", Colour::Red.paint("previous errors were found"));
+                return vec![];
+            },
+            Result::Ok(wasm_contents) => {
+                // println!("{}", Colour::Green.paint("Everything went okay, circom safe"));
+                return wasm_contents;
+            }
+        }
+    } else {
+        return vec![0];
+    }
+}
+
 fn start_compiler(file_name: String, link_libraries: Vec<String>, link_libraries_sources: Vec<String>, config: CircuitConfig) -> Result<Vec<u8>, Vec<String>> {
     use execution_wasm::ExecutionConfig;
     let (mut program_archive, warnings) = parser_wasm::parse_project(file_name, link_libraries, link_libraries_sources)?;
     let parse_report = type_analysis_wasm::analyse_project(&mut program_archive)?;
-    let config = ExecutionConfig {
+    let execution_config = ExecutionConfig {
         no_rounds: 0,
         flag_p: false,
         flag_s: false,
@@ -96,24 +134,19 @@ fn start_compiler(file_name: String, link_libraries: Vec<String>, link_libraries
         flag_old_heuristics: false,
         flag_verbose: false,
         inspect_constraints_flag: false,
-        r1cs_flag: false,
         json_constraint_flag: false,
         json_substitution_flag: false,
         sym_flag: false,
         sym: "".to_string(),
-        r1cs: "".to_string(),
         json_constraints: "".to_string(),
-        prime: config.prime,        
+        prime: config.prime,
     };
-    let circuit = execution_wasm::execute_project(program_archive, config)?;
+    let (_, circuit) = execution_wasm::execute_project(program_archive, execution_config)?;
     let compilation_config = CompilerConfig {
         vcp: circuit,
         debug_output: false,
-        // c_flag: user_input.c_flag(),
         c_flag: false,
-        // wasm_flag: user_input.wasm_flag(),
         wasm_flag: true,
-        // wat_flag: user_input.wat_flag(),
         wat_flag: false,
         js_folder: "".to_string(),
         wasm_name: "".to_string(),
@@ -150,4 +183,42 @@ fn start_parser(file_name: String, link_libraries: Vec<String>, link_libraries_s
         analysis_warnings.push(warns.to_string());
     }
     Result::Ok(analysis_warnings)
+}
+
+fn start_r1cs(file_name: String, link_libraries: Vec<String>, link_libraries_sources: Vec<String>, config: CircuitConfig) -> Result<Vec<u8>, Vec<String>> {
+    use execution_wasm::ExecutionConfig;
+    let (mut program_archive, warnings) = parser_wasm::parse_project(file_name, link_libraries, link_libraries_sources)?;
+    let parse_report = type_analysis_wasm::analyse_project(&mut program_archive)?;
+    let execution_config = ExecutionConfig {
+        no_rounds: 0,
+        flag_p: false,
+        flag_s: false,
+        flag_f: false,
+        flag_old_heuristics: false,
+        flag_verbose: false,
+        inspect_constraints_flag: false,
+        json_constraint_flag: false,
+        json_substitution_flag: false,
+        sym_flag: false,
+        sym: "".to_string(),
+        json_constraints: "".to_string(),
+        prime: config.prime,
+    };
+    let (exporter, circuit) = execution_wasm::execute_project(program_archive, execution_config)?;
+    let r1cs_details = generate_output_r1cs(exporter.as_ref(), program_archive.custom_gates)?;
+
+    // match compilation_details {
+    //     Result::Err(mut report) => {
+    //         for rp in parse_report.iter() {
+    //             report.push(rp.to_string());
+    //         }
+    //         for warns in warnings.iter()  {
+    //             report.push(warns.to_string());
+    //         }
+    //         return Err(report);
+    //     }
+    //     Result::Ok(wasm_contents) => {
+    //         return Result::Ok(wasm_contents);
+    //     }
+    // }
 }
